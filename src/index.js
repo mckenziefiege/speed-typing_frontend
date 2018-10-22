9e3aa706124584fdd7c1c41bb7b867057533ec15
jkhaha <jha2@wellesley.edu>
2018-10-22 16:21:59 -0400

    put random prompt on page

---
+++ src/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const promptDiv = document.querySelector(".prompt-div")
+  fetchMainPrompt()
+
+
+  function fetchMainPrompt() {
+  fetch('http://localhost:3000/prompts')
+  .then(res => res.json())
+  .then(putPromptOnPage)
+  }
+
+  function putPromptOnPage (prompts) {
+    let randomPrompt = prompts[Math.floor(Math.random()*prompts.length)];
+    const p = document.createElement('p')
+    p.innerText = randomPrompt.content
+    promptDiv.append(p)
+  }
+})
