fe025500cb47948825d7172786f9b4d2afd72cb6
jkhaha <jha2@wellesley.edu>
2018-10-23 11:35:54 -0400

    textbox for user

--- src/index.js
+++ src/index.js
@@ -12,14 +12,43 @@ document.addEventListener('DOMContentLoaded', () => {
   function putPromptOnPage (prompts) {
     let randomPrompt = prompts[Math.floor(Math.random()*prompts.length)];
     const p = document.createElement('p')
-    p.innerText = randomPrompt.content
-    promptDiv.append(p)
     let spaces = randomPrompt.content.split(" ")
     spaces.forEach (function(word) {
 
       let punctuationSplit = word.split("[.,!?:;’\“-]+\\s*")
-
-        })
+      const span = document.createElement('span')
+      span.innerHTML = ` ${punctuationSplit} `
+      p.append(span)
+      promptDiv.append(p)
     })
+  }
+  let userTextbox = document.getElementById('user-textbox')
+  userTextbox.addEventListener('keydown', function(event) {
+    if (event.keyCode == 32) {
+      event.target.innerHTML = wrapWords(this.innerText)
+      setCaretLast(this.id)
+      console.log(this.innerHTML)
+    }
+  })
+
+  function wrapWords (text) {
+    let splitText = text.split(' ')
+    let output = []
+    let count = splitText.length
+    for (let i = 0; i < count; i++) {
+      output[i] = "<span id=\"word-"+i+"\">"+splitText[i]+"</span>"
+    }
+    return output.join(' ')
+}
+
+function setCaretLast(el) {
+    // let el = document.getElementById(el);
+    let range = document.createRange();
+    let sel = window.getSelection();
+    range.setStart(el.childNodes[el.childNodes.length-1], 1);
+    range.collapse(true);
+    sel.removeAllRanges();
+    sel.addRange(range);
+    el.focus();
 }
 })
